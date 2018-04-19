1323bca3bd43a40c542582ff31ad29c3c600a9ce
mkhuda <mhmmd.huda@gmail.com>
2018-04-19 17:27:55 +0700

    import bootstrap & jquery modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 var m = require("mithril")
+var $ = require('jquery');
+window.$ = $;
+require("bootstrap")
+require('bootstrap/dist/css/bootstrap.min.css')
 var MyComponent = require("./components/MyComponent")
 
 m.render(document.body, <MyComponent />)
