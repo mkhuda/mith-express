0da65bd4c3470e5db521d6895effbc2b11d3ba31
mkhuda <mhmmd.huda@gmail.com>
2018-04-20 00:31:01 +0700

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+var m = require("mithril")
+
+module.exports = {
+  view: (vnode) => {
+    return(
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Home</a>
+        </nav>
+      </div>
+    )
+  }
+}
