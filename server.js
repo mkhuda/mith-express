c30f24f243d1d26bb27d678e15fde0bb903bca70
mkhuda <mhmmd.huda@gmail.com>
2018-04-19 17:27:11 +0700

    remove backslash from serve static

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use('/', express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
