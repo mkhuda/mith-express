ccfd6132155d5f824b52e04d90d91d3880db15a1
mkhuda <mhmmd.huda@gmail.com>
2018-04-19 17:28:27 +0700

    config webpack to load css from bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,16 @@ module.exports = {
         filename: 'bundle.js',
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ],
     }
 }
