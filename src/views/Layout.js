acc56813d3cd927939cd89a37001c4004be39c07
mkhuda <mhmmd.huda@gmail.com>
2018-04-24 00:20:22 +0700

    set title for client and server side

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,29 +1,38 @@
 const m = require('mithril');
 
+function getTitle(title) {
+  return `${title} | Mithril Express`;
+}
+
 const layoutClient = {
+  oncreate(vnode) {
+    document.title = getTitle(vnode.attrs.pageTitle);
+  },
   view(vnode) {
     return vnode.children;
   },
 };
 
 const layoutServer = {
+  oninit(vnode) {
+    this.pageTitle = vnode.attrs.pageTitle;
+  },
   view(vnode) {
     return [
-      m(
-        '!doctype[html]',
-        m('html', [
-          m('head', [
-            m('meta', { charset: 'utf-8' }),
-            m('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }),
-            m('meta', { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' }),
-            m('title', ''),
-          ]),
-          m('body.server', [
-            vnode.children,
-            m('script', { src: './bundle.js' }),
-          ]),
+      m('!DOCTYPE[html]'),
+      m('html', [
+        m('head', [
+          m('meta', { charset: 'utf-8' }),
+          m('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }),
+          m('meta', { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' }),
+          m('title', getTitle(this.pageTitle)),
+          m('link', { rel: 'stylesheet', href: 'application.css' }),
         ]),
-      ),
+        m('body.server', [
+          vnode.children,
+          m('script', { src: 'bundle.js' }),
+        ]),
+      ]),
     ];
   },
 };
