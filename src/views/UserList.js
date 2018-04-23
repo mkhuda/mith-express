d6909232746bad801fcefa9f9461360cb2c5d37b
mkhuda <mhmmd.huda@gmail.com>
2018-04-24 00:56:08 +0700

    remove onmatch

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -3,29 +3,25 @@ const Layout = require('./Layout');
 const Footer = require('../components/Footer');
 
 module.exports = {
-  onmatch(params, url) {
-    return {
-      oninit() {
-        this.state = { pageTitle: 'User List', params, url };
-      },
-      view() {
-        return [
-          m(Layout, this.state, m('.container', m('.columns', [
-            m('.column col-6 col-mx-auto', [
-              m('br'), m('br'), m('br'), m('br'), m('br'),
-              m('.text-center', m('h2', 'And this is the other page')),
-              m('br'),
-              m('.text-center', m(
-                'a[class=btn][href=/]',
-                { oncreate: m.route.link },
-                m('i.icon icon-arrow-left'),
-                'Back to home page',
-              )),
-              m(Footer),
-            ]),
-          ]))),
-        ];
-      },
-    };
+  oninit() {
+    this.state = { pageTitle: 'User List' };
+  },
+  view() {
+    return [
+      m(Layout, this.state, m('.container', m('.columns', [
+        m('.column col-6 col-mx-auto', [
+          m('br'), m('br'), m('br'), m('br'), m('br'),
+          m('.text-center', m('h2', 'And this is the other page')),
+          m('br'),
+          m('.text-center', m(
+            'a[class=btn][href=/]',
+            { oncreate: m.route.link },
+            m('i.icon icon-arrow-left'),
+            'Back to home page',
+          )),
+          m(Footer),
+        ]),
+      ]))),
+    ];
   },
 };
--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -3,27 +3,23 @@ const Layout = require('./Layout');
 const Footer = require('../components/Footer');
 
 module.exports = {
-  onmatch(params, url) {
-    return {
-      oninit() {
-        this.state = { pageTitle: 'Welcome', params, url };
-      },
-      view() {
-        return [
-          m(Layout, this.state, m('.container', m('.columns', [
-            m('.column col-6 col-mx-auto', [
-              m('br'), m('br'), m('br'), m('br'), m('br'),
-              m('.text-center', m('h1', 'Welcome to Mithrill App')),
-              m('br'),
-              m('.text-center', [
-                m('a[class=btn btn-primary][href=/user]', { oncreate: m.route.link }, 'See other page'),
-              ]),
-              m(Footer),
-            ]),
-          ]))),
-        ];
-      },
-    };
+  oninit() {
+    this.state = { pageTitle: 'Welcome' };
+  },
+  view() {
+    return [
+      m(Layout, this.state, m('.container', m('.columns', [
+        m('.column col-6 col-mx-auto', [
+          m('br'), m('br'), m('br'), m('br'), m('br'),
+          m('.text-center', m('h1', 'Welcome to Mithrill App')),
+          m('br'),
+          m('.text-center', [
+            m('a[class=btn btn-primary][href=/user]', { oncreate: m.route.link }, 'See other page'),
+          ]),
+          m(Footer),
+        ]),
+      ]))),
+    ];
   },
 };
 
