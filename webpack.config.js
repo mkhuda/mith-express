e8ef10c4e7c10edb02c1b2b27fff3bcaa480d8a1
mkhuda <mhmmd.huda@gmail.com>
2018-04-25 08:10:01 +0700

    webpack css-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
-const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const SuppressEntryChunksPlugin = require('./libs/SuppressEntryChunksPlugin');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+const cssnano = require('cssnano');
 
 const extractSass = new ExtractTextPlugin({
   filename: '[name].css',
@@ -14,8 +16,8 @@ module.exports = {
   name: 'client',
   target: 'web',
   entry: {
-    'bundle': './src/index.js',
-    'application': './spectre/style.scss',
+    bundle: './src/index.js',
+    application: './spectre/style.scss',
   },
   output: {
     path: path.resolve(__dirname, './build'),
@@ -27,31 +29,24 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-      },{
+      }, {
         test: /\.scss$/,
         use: extractSass.extract({
           use: [{
+            loader: 'css-loader',
+          }, {
             loader: 'postcss-loader',
             options: {
-              ident: 'postcss',
-              plugins: (loader) => [
-                require('autoprefixer')(),
-                require('precss')(),
-                require('cssnano')({
-                  preset: ['default', {
-                    discardComments: {
-                      removeAll: true,
-                    },
-                  }]
-                }),
-              ],
+              plugins() {
+                return [autoprefixer, precss, cssnano];
+              },
             },
-          },{
+          }, {
             loader: 'sass-loader',
           }],
           fallback: 'style-loader',
         }),
-      },{
+      }, {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader',
