b05d82de7a0075beac60eb885a34761839e09742
mkhuda <mhmmd.huda@gmail.com>
2018-04-26 17:10:16 +0700

    iterate User map

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,15 +1,12 @@
 const m = require('mithril');
 const Layout = require('./Layout');
 const Footer = require('../components/Footer');
-const { getUsers } = require('../actions/user');
+const User = require('../actions/User');
 
 module.exports = {
   oninit(vnode) {
     vnode.state.pageTitle = 'User List';
-    getUsers()
-      .then((data) => {
-        Object.assign(vnode.state, { data });
-      });
+    User.loadList();
   },
   view(vnode) {
     return [
@@ -19,9 +16,13 @@ module.exports = {
           m('.text-center', m('h2', 'And this is the other page')),
           m('br'),
           m('.text-center', [
-            m('a[class=btn][href=/]', { oncreate: m.route.link }, 'Back to home page'),
             m('br'),
-            m('.column', JSON.stringify(vnode.state.data)),
+            m('.columns', m('.column col-12 col-mx-auto', [
+              User.list.map(user => m('span.chip', user)),
+            ])),
+            m('br'),
+            m('br'),
+            m('a[class=btn btn-sm][href=/]', { oncreate: m.route.link }, m('i.icon icon-arrow-left'), 'Back to home page'),
           ]),
           m(Footer),
         ]),
