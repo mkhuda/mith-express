642769ab732e6cb1d8e54330f3e395b53b492b1c
mkhuda <mhmmd.huda@gmail.com>
2018-04-26 15:09:39 +0700

    add global window XMLHttpRequest for server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('mithril/test-utils/browserMock')(global);
+global.window.XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest;
+
 const app = require('./server/index');
 
 const port = process.env.PORT || 5000;
