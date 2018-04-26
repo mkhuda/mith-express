db43a1aa3da217561f78cdd0c381fe5eb3515641
mkhuda <mhmmd.huda@gmail.com>
2018-04-26 15:47:59 +0700

    localurl and port variable on action

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,13 +1,14 @@
 const m = require('mithril');
 
-const localUrl = window.location.protocol + '//' + (process.browser ? window.location.host : 'localhost:5000');
-// const localUrl = `${window.location.protocol}//${(process.browser ? window.location.host : 'localhost:5000')}`;
+const port = process.env.PORT || 5000;
+const localHost = `127.0.0.1:${port}`;
+const localUrl = `${window.location.protocol}//${(process.browser ? window.location.host : localHost)}`;
 
 function getUsers() {
   return m.request({
     method: 'GET',
     url: `${localUrl}/api/users`,
-  })
+  });
 }
 
 module.exports = { getUsers };
