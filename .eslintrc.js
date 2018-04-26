743c9d66b95a7de40510e52a401c7d70ff5a73ad
mkhuda <mhmmd.huda@gmail.com>
2018-04-26 15:07:11 +0700

    some rules on eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   'env': {
     'browser': true
   },
+  'rules': {
+    'no-param-reassign': 0,
+    'no-unused-vars': [1, {'vars': 'local', 'args': 'none'}]
+  },
   'extends': 'airbnb-base',
   'parser': 'babel-eslint',
   'parserOptions': {
